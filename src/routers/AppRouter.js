ffc889be54c5c5948b5b9eef0b8b6f9392a29cdf
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-19 22:40:50 -0300

    exact

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 export default function AppRouter() {
   return (
     <Router>
-      <Switch>{/* selecciona la primera ruta que coincide */}
+      <Switch>
         <Route path="/about">
           <h1>About</h1>
         </Route>
         <Route path="/contact">
           <h1>Contact</h1>
         </Route>
-        <Route path="/">{/* si home estubiese al principio, la eligiria siempre */}
+        <Route exact path="/">{/* con exact la selecciona */}
           <h1>Home</h1>
         </Route>
       </Switch>
