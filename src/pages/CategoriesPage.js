97847648b94e2d6e464d1b9584262bf07a117933
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-20 00:23:49 -0300

    history-contact

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,18 +1,27 @@
+import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 export default function CategoriesPage() {
   const location = useLocation();
+  const history = useHistory();
   const query = new URLSearchParams(location.search);
-  const skip = query.get("skip")
-  const limit = query.get("limit")
+  const skip = query.get("skip") || 0; //por defecto 0
+  const limit = query.get("limit") || 15; //por defecto 15
   console.log(skip);
   console.log(limit);
-  
+
+  const handleNext = () => {
+    query.set("skip", 100);
+    query.set("limit", 200);
+    history.push({ pathname: "/contact" });
+  };
+
   return (
     <div>
       <h1>CategoriesPage:</h1>
       <h2>{skip}</h2>
       <h2>{limit}</h2>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 }
