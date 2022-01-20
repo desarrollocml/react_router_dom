64923e7f521179ad2003f335aff2aa8b614ae735
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-20 01:28:22 -0300

    redirect

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,5 +1,19 @@
+import { Redirect } from "react-router-dom";
 import { Route } from "react-router-dom";
 
-export default function PrivateRoute(props) {
-  return <Route {...props} />;
+//const user = null;
+const user = {id:1,username:"luis50"}
+export default function PrivateRoute({component:Component,
+     ...rest}) {
+  return (
+      <Route {...rest} >
+          {
+           user
+           ?   
+           <Component></Component>
+           :
+           <Redirect to="/login"></Redirect>
+          }
+      </Route>
+  )
 }
