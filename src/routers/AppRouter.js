3d38e5150994c0f09a84380bb171dbd43893388b
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-19 22:35:35 -0300

    comentarios-switch

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 export default function AppRouter() {
   return (
     <Router>
-      <Switch>
+      <Switch>{/* selecciona la primera ruta que coincide */}
         <Route path="/about">
           <h1>About</h1>
         </Route>
         <Route path="/contact">
           <h1>Contact</h1>
         </Route>
-        <Route path="/">
+        <Route path="/">{/* si home estubiese al principio, la eligiria siempre */}
           <h1>Home</h1>
         </Route>
       </Switch>
