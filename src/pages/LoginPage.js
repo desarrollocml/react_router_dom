efd3b21c9cfbad2a9360f4a5c5596858e045ad65
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-20 22:36:08 -0300

    pagina previa

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,22 @@
+import { useLocation } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import useAuth from "../auth/useAuth";
 
 export default function LoginPage() {
   const auth = useAuth();
   const history = useHistory();
+  const location = useLocation();
+  const previusObjectURl = location.state?.from;
+  console.log("-->",previusObjectURl)
   const handleLogin = () => {
     auth.login();
-    history.push("/dashboard");
+    history.push(previusObjectURl);
   };
- 
+
   return (
     <div>
       <h1>LoginPage</h1>
       <button onClick={handleLogin}>Sigin</button>
-      
     </div>
   );
 }
--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,22 +1,20 @@
 import { Redirect } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Route } from "react-router-dom";
 import useAuth from "../auth/useAuth";
 
 //const user = null;
 //const user = {id:1,username:"luis50"}
-export default function PrivateRoute({component:Component,
-     ...rest}) {
-        // const {user,logn,logout}=useAuth()
-         const auth =useAuth()
+export default function PrivateRoute({ component: Component, ...rest }) {
+  const location = useLocation();
+  const auth = useAuth();
   return (
-      <Route {...rest} >
-          {
-           auth.isLogged()
-           ?   
-           <Component></Component>
-           :
-           <Redirect to="/login"></Redirect>
-          }
-      </Route>
-  )
+    <Route {...rest}>
+      {auth.isLogged() ? (
+        <Component></Component>
+      ) : (
+        <Redirect to={{pathname:"/login",state:{from:location}}}></Redirect>
+      )}
+    </Route>
+  );
 }
